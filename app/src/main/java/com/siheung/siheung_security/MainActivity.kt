693b69f160a637d4b8a587ac63c888c8a0f129c3
kimcurly <sanbonai0728@gmail.com>
2023-11-29 23:59:09 +0900

    View Binding Setting

--- app/src/main/java/com/siheung/siheung_security/MainActivity.kt
+++ app/src/main/java/com/siheung/siheung_security/MainActivity.kt
@@ -10,37 +10,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.siheung.siheung_security.databinding.ActivityMainBinding
 import com.siheung.siheung_security.ui.theme.SiheungsecurityTheme
 
+
+private lateinit var binding : ActivityMainBinding
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            SiheungsecurityTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SiheungsecurityTheme {
-        Greeting("Android")
     }
 }
