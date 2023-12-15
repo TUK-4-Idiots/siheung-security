7cd85af767e444eb76161695b2837b0dd7e90ce3
한혜민 <hanhyemin@hanhyemin-ui-MacBookPro.local>
2023-12-16 02:32:53 +0900

    feat: 스플래시 화면 및 온보딩 화면 구현

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,6 +70,10 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    //온보딩 화면 라이브러리
     implementation("androidx.viewpager2:viewpager2:1.0.0")
     implementation("com.tbuonomo:dotsindicator:5.0")
+    //스플래시 화면 라이브러리
+    implementation ("pl.droidsonroids.gif:android-gif-drawable:1.2.20")
+
 }
