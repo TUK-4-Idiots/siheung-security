956af6a4ae0d4478128542286c92516530441370
kimcurly <sanbonai0728@gmail.com>
2023-12-17 00:51:58 +0900

    feat: NextButton 프래그먼트화

--- app/src/main/java/com/siheung/siheung_security/fragments/FragmentButton.kt
+++ app/src/main/java/com/siheung/siheung_security/fragments/FragmentButton.kt
@@ -1,24 +1,48 @@
 package com.siheung.siheung_security.fragments
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.siheung.siheung_security.databinding.FragmentProgressBinding
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Observer
+import com.siheung.siheung_security.SignUpViewModel
+import com.siheung.siheung_security.R
+import com.siheung.siheung_security.databinding.FragmentButtonBinding
 
-class ProgressFragment: Fragment() {
-    private var _binding: FragmentProgressBinding? = null
+class FragmentButton: Fragment() {
+    private var _binding: FragmentButtonBinding? = null
     private val binding get() = _binding!!
+
+    private val viewModel: SignUpViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        _binding = FragmentProgressBinding.inflate(layoutInflater, container, false)
+        _binding = FragmentButtonBinding.inflate(layoutInflater, container, false)
+
+        viewModel.getLength().observe(viewLifecycleOwner, Observer { length ->
+            Log.d("tag", "$length")
+            if (length >= 2) {
+                binding.nextButton.isEnabled = true
+                binding.nextButton.setBackgroundResource(R.drawable.button_background_blue)
+            }
+            else {
+                binding.nextButton.isEnabled = false
+                binding.nextButton.setBackgroundResource(R.drawable.button_background_disabled)
+            }
+        })
+
+        binding.nextButton.setOnClickListener {
+            viewModel.nextStep()
+        }
         return binding.root
     }
+
 
     override fun onStart() {
         super.onStart()
